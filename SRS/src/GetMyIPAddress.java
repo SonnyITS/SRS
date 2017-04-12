536ca9cd9b389674c7b6733b69c9fa3beaea4304
user <user@137.99.150.138>
2017-04-11 20:23:36 -0400

    In case we decide to do use the IPAdress. I have another class but I won't upload it unless we need it.

---
+++ SRS/src/GetMyIPAddress.java
@@ -0,0 +1,25 @@
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Enumeration;
+
+public class GetMyIPAddress {
+	public static void main(String gks[]) throws SocketException {
+		Enumeration e = NetworkInterface.getNetworkInterfaces();
+		int ctr = 0;
+		while (e.hasMoreElements()) {
+			NetworkInterface n = (NetworkInterface) e.nextElement();
+			Enumeration ee = n.getInetAddresses();
+			while (ee.hasMoreElements() && ctr < 3) {
+				ctr++;
+				if (ctr == 3)
+					break;
+				InetAddress i = (InetAddress) ee.nextElement();
+				if (ctr == 2)
+					System.out.println(i.getHostAddress());
+
+			}
+		}
+	}
+
+}
